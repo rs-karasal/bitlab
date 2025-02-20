681c111c03bb30b8723785384baf59397a358612
millennial <ereskarasal94@gmail.com>
2025-02-20 08:18:32 +0500

    core: documentation comments

--- config/config.go
+++ config/config.go
@@ -16,21 +16,22 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	err := godotenv.Load()
+	err := godotenv.Load() // загрузка вирт окружения
 	if err != nil {
 		return nil, err
 	}
 
 	return &Config{
 		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5433"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "postgres"),
-		DBName:     getEnv("DB_NAME", "postgres"),
-		DBSSLMode:  getEnv("DB_SSL_MODE", "disable"),
+		DBPort:     getEnv("DB_PORT", "5555"),
+		DBUser:     getEnv("DB_USER", "ps"),
+		DBPassword: getEnv("DB_PASSWORD", "ps"),
+		DBName:     getEnv("DB_NAME", "ps"),
+		DBSSLMode:  getEnv("DB_SSL_MODE", "enable"),
 	}, nil
 }
 
+// НЕОБЯЗАТЕЛЬНО, НО УДОБНО
 func getEnv(key, defaultValue string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
